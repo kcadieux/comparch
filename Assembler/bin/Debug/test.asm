c2ee49dafe40656b99c5132ecb570cab191ec752
maximegregoire <maximegregoireeng@gmail.com>
2015-02-16 20:28:03 -0500

    Added support for jump and branch instructions

--- Assembler/bin/Debug/test.asm
+++ Assembler/bin/Debug/test.asm
@@ -1,5 +1,6 @@
 addi $t6 $t6 103
 andi $t4 $t5 -33
+LABEL1:
 slti $t4 $t5 -33
 ori $t4 $t5 -33
 xori $t4 $t5 -33
@@ -8,8 +9,10 @@ lw $t4, $t5, -33
 lb $t4, $t5, -33
 sw $t4, $t5, -33
 sb $t4, $t5, -33
-beq $t4, $t5, -33
-bne $t4, $t5, -33
+LABEL2 :
+beq $t4, $t5, LABEL2
+bne $t4, $t5, LABEL1
+bne $t4, $t5, LABEL3
 add $t5, $t6, $t7
 sub $at, $v0, $v1 ; some test
 mult $at, $t5 ;more test
@@ -17,6 +20,7 @@ div $at, $t5			;orly
 slt $at, $t5, $t6 ;orly
 and $at, $t5, $t6
 or $at, $t5, $t6
+LABEL3:
 nor $at, $t5, $t6
 xor $at, $t5, $t6
 mfhi $at
@@ -26,5 +30,5 @@ srl $at,$t5, 15 ;Due to confusing specs, implemented both srl and slr
 slr $at,$t5, 15 ;Due to confusing specs, implemented both srl and slr
 sra $at $t5, 31
 jr $ra
-j 1023
-jal 1023
+j LABEL1
+jal LABEL2
