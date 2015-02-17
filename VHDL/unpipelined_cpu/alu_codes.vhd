ae7c0b4e4114a726fe7ef0b70a529154bae94de1
Kevin Cadieux <kcadieux@gmail.com>
2015-02-16 20:40:52 -0500

    tabulation fix

--- VHDL/unpipelined_cpu/alu_codes.vhd
+++ VHDL/unpipelined_cpu/alu_codes.vhd
@@ -4,21 +4,21 @@ use ieee.std_logic_1164.all; -- allows use of the std_logic_vector type
 use ieee.numeric_std.all; -- allows use of the unsigned type
 
 PACKAGE alu_codes IS
-	--Arithmetic function codes
-	CONSTANT	FUNCT_ADD 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#20#, 6));
-	CONSTANT	FUNCT_SUB 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#22#, 6));
-	CONSTANT	FUNCT_MULT 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#18#, 6));
-	CONSTANT	FUNCT_DIV 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#1A#, 6));
-	CONSTANT	FUNCT_SLT 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#2A#, 6));
-	
-	--Logical function codes
-	CONSTANT	FUNCT_AND 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#24#, 6));
-	CONSTANT	FUNCT_OR 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#25#, 6));
-	CONSTANT	FUNCT_NOR 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#27#, 6));
-	CONSTANT	FUNCT_XOR 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#26#, 6));
-	
-	--Shift function codes
-	CONSTANT	FUNCT_SLL 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#00#, 6));
-	CONSTANT	FUNCT_SRL 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#02#, 6));
-	CONSTANT	FUNCT_SRA 	: STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#03#, 6));
+   --Arithmetic function codes
+   CONSTANT   FUNCT_ADD    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#20#, 6));
+   CONSTANT   FUNCT_SUB    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#22#, 6));
+   CONSTANT   FUNCT_MULT    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#18#, 6));
+   CONSTANT   FUNCT_DIV    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#1A#, 6));
+   CONSTANT   FUNCT_SLT    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#2A#, 6));
+   
+   --Logical function codes
+   CONSTANT   FUNCT_AND    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#24#, 6));
+   CONSTANT   FUNCT_OR    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#25#, 6));
+   CONSTANT   FUNCT_NOR    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#27#, 6));
+   CONSTANT   FUNCT_XOR    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#26#, 6));
+   
+   --Shift function codes
+   CONSTANT   FUNCT_SLL    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#00#, 6));
+   CONSTANT   FUNCT_SRL    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#02#, 6));
+   CONSTANT   FUNCT_SRA    : STD_LOGIC_VECTOR(5 DOWNTO 0) := std_logic_vector(to_unsigned(16#03#, 6));
 END alu_codes;
--- VHDL/unpipelined_cpu/unpipelined_cpu.vhd
+++ VHDL/unpipelined_cpu/unpipelined_cpu.vhd
@@ -5,9 +5,9 @@
 --
 -- Copyright (C) 2015 Kevin Cadieux
 --
--- Version:	1.0 
--- Author: 	Kevin Cadieux (kevin.cadieux@mail.mcgill.ca)
--- Date:		February 16, 2015
+-- Version:   1.0 
+-- Author:    Kevin Cadieux (kevin.cadieux@mail.mcgill.ca)
+-- Date:      February 16, 2015
 
 library ieee;
 
@@ -15,19 +15,19 @@ use ieee.std_logic_1164.all; -- allows use of the std_logic_vector type
 use ieee.numeric_std.all; -- allows use of the unsigned type
 
 ENTITY unpipelined_cpu IS
-	
-	PORT (
-		clk:	 			IN STD_LOGIC
-	);
-	
-	
+   
+   PORT (
+      clk:             IN STD_LOGIC
+   );
+   
+   
 END unpipelined_cpu;
 
 ARCHITECTURE rtl OF unpipelined_cpu IS
-	
+   
 BEGIN
-	
-	
-	
-	
+   
+   
+   
+   
 END rtl;
