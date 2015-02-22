bddb554afc2c0832b01f1e1dd11dd4be998e42c9
Kevin Cadieux <kcadieux@gmail.com>
2015-02-21 22:09:57 -0500

    Other tests from myCourses with added asrti instructions to make sure execution happens as expected

---
+++ CPUS/unpipelined_cpu/tests/MinMaxAvg.asm
@@ -0,0 +1,160 @@
+#             Min/Max/Average
+#
+# This routine computes the min/max and average of
+# a variable length array.
+#
+
+#Array:		.word	34, 78, 43, 67, 91, 56, 25, 69
+
+	addi $11,  $0, 2000  	# initializing the beginning of Data Section address in memory
+	addi $15, $0, 4 		# word size in byte
+	
+	addi $10, $0, 0
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,34 
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 1
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,78 
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 2
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,43 
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 3
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,67 
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 4			# word number 
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,91 			# Saved value into memory
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 5			# word number 
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,56 			# Saved value into memory
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 6			# word number 
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,25 			# Saved value into memory
+	sw	 $2, 0($13)
+	
+	addi $10, $0, 7			# word number 
+	mult $10, $15			# $lo=4*$10, for word alignment 
+	mflo $12				# assume small numbers
+	add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+	addi $2,$0,69 			# Saved value into memory
+	sw	 $2, 0($13)
+	
+	#Assert that the array has been stored correctly: 34, 78, 43, 67, 91, 56, 25, 69
+	lw    $10 0($11)
+	asrti $10 34
+	lw    $10 4($11)
+	asrti $10 78
+	lw    $10 8($11)
+	asrti $10 43
+	lw    $10 12($11)
+	asrti $10 67
+	lw    $10 16($11)
+	asrti $10 91
+	lw    $10 20($11)
+	asrti $10 56
+	lw    $10 24($11)
+	asrti $10 25
+	lw    $10 28($11)
+	asrti $10 69
+	
+	
+
+Main:	add	  $1, $0, $11	# init starting ptr
+		addi  $2, $0, 8	    # init length
+		add	  $10, $31, $0	# preserve return addres
+		jal	  MinMaxAvg	    # call routine
+		
+		asrti $3 25         # Check min
+		asrti $4 91         # Check max
+		asrti $5 57			# Check average
+		
+		j	  EoP		    # return to os
+
+# inputs: $1= array ptr, $2= array length, $31 return address
+# outputs: $3= min, $4= max, $5= avg
+# working: $6= end ptr, $7= current element, $8= predicate
+
+MinMaxAvg: lw $3, 0($1)	# load init min
+		lw	  $4, 0($1)	# load init max
+		lw	  $5, 0($1)	# load init sum
+		sll	  $6, $2, 2	# length x 4
+		add	  $6, $6, $1	# end = start + 4 x length
+		addi  $1, $1, 4	# increment ptr
+Loop:	lw	  $7, 0($1)	# load next element
+		slt	  $8, $7, $3	# if current >= min
+		beq	  $8, $0, Skip1	# then skip update
+		add	  $3, $7, $0	# update min
+Skip1:	slt	  $8, $4, $7	# if current <= max
+		beq	  $8, $0, Skip2	# then skip update
+		add	  $4, $7, $0	# update max
+Skip2:	add	  $5, $5, $7	# add element to sum
+		addi  $1, $1, 4	# point to next element
+		bne	  $1, $6, Loop	# if not done, loop
+		
+		div	  $5, $2		# compute avg
+		mflo  $5
+		
+		addi  $11,  $0, 3000  	# initializing the beginning of Section for saving data in memory
+		addi  $15, $0, 4 		# word size in byte
+	
+		addi  $10, $0, 0
+		mult  $10, $15			# $lo=4*$10, for word alignment 
+		mflo  $12				# assume small numbers
+		add   $13, $11, $12 	# Make data pointer [2000+($10)*4]
+		add   $2,$0,$3			#Min Value 
+		sw	  $2, 0($13)
+		
+		addi  $10, $0, 1
+		mult  $10, $15			# $lo=4*$10, for word alignment 
+		mflo  $12				# assume small numbers
+		add   $13, $11, $12 	# Make data pointer [2000+($10)*4]
+		add   $2,$0,$4			# Man Value 
+		sw	  $2, 0($13)
+	
+		
+		
+		addi $10, $0, 2
+		mult $10, $15			# $lo=4*$10, for word alignment 
+		mflo $12				# assume small numbers
+		add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
+		add $2,$0,$5			# Avg. Value 
+		sw	 $2, 0($13)
+		
+		jr   $ra
+			
+		#Assert that the correct answer was written to memory
+		
+EoP:	lw    $10 0($11)
+	    asrti $10 25
+	    lw    $10 4($11)
+	    asrti $10 91
+	    lw    $10 8($11)
+	    asrti $10 57
+		
+        halt
