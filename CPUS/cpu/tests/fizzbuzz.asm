08c0926f0839eff1a3a915763a9e57aa983f269d
maximegregoire <maximegregoireeng@gmail.com>
2015-04-11 13:31:10 -0400

    Added fizzbuzz test and fix for liveCPU

--- CPUS/cpu/tests/fizzbuzz.asm
+++ CPUS/cpu/tests/fizzbuzz.asm
@@ -1,7 +1,8 @@
-# This is a linear search program. It searches the array for 
-# the answer to life, the universe and everything.
+# This is a fizzbuzz implementation. If a given number is divisible by 3,
+# the flag fizz is set. If it is divisible by 5, the flag buzz is set. If
+# it is divisible by 15, the flag fizzbuzz is set.
 # Our memory list is a series of words, terminated with -1
-# mem: .word 4, 5, 3, 2, 1, 8, 2, 2, 4, 86, 95, 123, 4, 13, 24, 52, 33, 34, 38, 31, 42, 13, 12, -1
+# mem: .word 4, 5, 3, 2, 15, 8, 25, 30, 9, 90, 60, 123, 120, 20, 24, 52, 33, 150, 300, 316, 315, -1
 ###########Setting the data memory ##############
  
  addi $11,  $0, 2000   # initializing the beginning of Data Section address in memory
@@ -39,7 +40,7 @@
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,1    # Saved value into memory
+ addi $2,$0,15    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 5   # word number 
@@ -53,35 +54,35 @@
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,2    # Saved value into memory
+ addi $2,$0,25    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 7   # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,2   # Saved value into memory
+ addi $2,$0,30   # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 8      # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,4   # Saved value into memory
+ addi $2,$0,9   # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 9   # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,86    # Saved value into memory
+ addi $2,$0,90    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 10  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,95    # Saved value into memory
+ addi $2,$0,60    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 11  # word number 
@@ -95,14 +96,14 @@
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,4    # Saved value into memory
+ addi $2,$0,120    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 14  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,13    # Saved value into memory
+ addi $2,$0,20    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 15  # word number 
@@ -130,45 +131,31 @@
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,34    # Saved value into memory
+ addi $2,$0,150    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 19  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,38    # Saved value into memory
+ addi $2,$0,300    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 20  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,31    # Saved value into memory
+ addi $2,$0,316    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 21  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,42    # Saved value into memory
+ addi $2,$0,315    # Saved value into memory
  sw  $2, 0($13)
  
  addi $10, $0, 22  # word number 
- mult $10, $15   # $10=4*$10, for word alignment 
- mflo $12    # assume small numbers
- add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,13    # Saved value into memory
- sw  $2, 0($13)
- 
- addi $10, $0, 23  # word number 
- mult $10, $15   # $10=4*$10, for word alignment 
- mflo $12    # assume small numbers
- add  $13, $11, $12   # Make data pointer [2000+($10)*4]
- addi $2,$0,52    # Saved value into memory
- sw  $2, 0($13)
- 
- addi $10, $0, 24  # word number 
  mult $10, $15   # $10=4*$10, for word alignment 
  mflo $12    # assume small numbers
  add  $13, $11, $12   # Make data pointer [2000+($10)*4]
@@ -179,25 +166,46 @@
  
 
 # Start of list processing:
-main:  addi $6,  $0, 2000  
-  addi   $20,$0,0         # Clear our 'found' flag
-  addi	 $16,$0,42		  # The number we are looking for
+  addi $6,  $0, 2000  
+  addi $20,$0,0         # Clear our 'fizz' flag
+  addi $21,$0,0         # Clear our 'buzz' flag
+  addi $22,$0,0         # Clear our 'fizzbuzz' flag  
+  addi $24,$0,1		    # Flag to signal the end of list
+  addi $25,$0,3			# Compare value for 'fizz'
+  addi $26,$0,5			# Compare value for 'buzz'
+  addi $27,$0,15		# Compare value for 'fizzbuzz'
+  
 # For each element of list: 
-loop: lw   $10,0($6)     # Load current value into $10
- 
-  beq $10, $16,found	# If value was found, our work here is done
- 
-  slti $21, $10, 0
-  addi $22, $0, 1
-  beq  $21,$22,done     # If end of list, our work here is done
- 
+loop: 
+  lw   $10,0($6)     # Load current value into $10
+  slti $23, $10, 0
+  beq  $23,$24,done     # If end of list, our work here is done
+  
+  div $10,$25		# divide by 3
+  mfhi $3 			# reminder of the division (modulo equivalence)
+  bne $3,$0,buzz # If not equal to 0, skip fizz flag
+  
+  addi $20,$20,1	# Add 1 to the fizz flag
+  
+buzz:
+  div $10,$26			# divide by 5
+  mfhi $3 				# reminder of the division (modulo equivalence)
+  bne $3,$0,fizzbuzz # If not equal to 0, skip fizz flag
+  addi $21,$21,1	# Add 1 to the buzz flag
+  
+fizzbuzz:
+  div $10,$27			# divide by 15
+  mfhi $3 				# reminder of the division (modulo equivalence)
+  bne $3,$0,nofizzbuzz  # If not equal to 0, skip fizz flag
+  addi $22,$22,1	# Add 1 to the fizzbuzz flag
+
+nofizzbuzz:
   addi $6,$6,4     # Advance $6 to next list element
   j    loop          # Compare this element now
- 
-# The answer was found
-found:   addi $20,$0,1	# Set our found flag
-  j    done
- 
+
 # Done
-done:  	asrti $20,1  # The value was found
-  halt
+done:
+asrti $20,13  # The amount of fizz (/3) = 13
+asrti $21,11  # The amount of buzz (/5) = 11
+asrti $22,8  # The amount of fizzbuzz (/15) = 8
+halt
